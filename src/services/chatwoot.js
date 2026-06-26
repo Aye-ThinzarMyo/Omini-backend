af6ec8831b83ba87b1ae8f05695d7cc12fcebe8d
Aye Thinzar Myo <ayethinzarmyo2019@gmail.com>
2026-06-26 15:09:31 +0630

    user create

--- src/services/chatwoot.js
+++ src/services/chatwoot.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 const BASE_URL = process.env.CHATWOOT_BASE_URL;
 const PLATFORM_TOKEN = process.env.CHATWOOT_PLATFORM_TOKEN;
@@ -6,22 +6,26 @@ const PLATFORM_TOKEN = process.env.CHATWOOT_PLATFORM_TOKEN;
 const chatwootApi = axios.create({
   baseURL: `${BASE_URL}/platform/api/v1`,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
     api_access_token: PLATFORM_TOKEN,
   },
 });
 
 export async function createChatwootUser({ name, email, password }) {
-  const { data } = await chatwootApi.post('/users', {
-    name, email, password, custom_attributes: {},
+  const { data } = await chatwootApi.post("/users", {
+    name,
+    display_name: name,
+    email,
+    password,
+    custom_attributes: {},
   });
-
+  console.log("data:::", data);
   return { chatwootId: data.id, apiKey: data.access_token };
 }
 
 export async function createChatwootAccount(chatwootUserId, accountName) {
   const { data } = await chatwootApi.post(`/users/${chatwootUserId}/accounts`, {
-    name: accountName || 'Default Account',
+    name: accountName || "Default Account",
   });
 
   return { accountId: data.id };
