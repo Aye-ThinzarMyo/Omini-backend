a3e243712cfbcd8ecfffe63835173e5bc028de52
Aye Thinzar Myo <ayethinzarmyo2019@gmail.com>
2026-07-09 14:49:36 +0630

    generate download url form freepbx

--- src/routes/freepbx.js
+++ src/routes/freepbx.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getCallChart, getCallRecordingsList } from "../controllers/freepbxController";
+import { getCallChart, getCallRecordingsList, getRecordingFile } from "../controllers/freepbxController";
 
 const router = Router();
 
 router.get("/calls/chart", getCallChart);
 router.get("/calls/recordings", getCallRecordingsList);
+router.get("/recordings/file", getRecordingFile);
 
 export default router;
